1fdf24e8ff505c33e5b07a9cceaa851c677cdfc5
anugrah18 <anugrah18@gmail>
2023-04-10 16:08:23 -0700

    Added remember me checkbox

--- src/components/Homepage/Login/Login.js
+++ src/components/Homepage/Login/Login.js
@@ -18,13 +18,41 @@ export default function Login() {
         <input
           className="w-full py-2 bg-emerald-700 text-white px-1 outline-none mb-4"
           type="password"
+          placeholder="at least 8 characters"
         />
+
+        <div class="flex items-center mr-4">
+          <input
+            id="remember-me-checkbox"
+            type="checkbox"
+            value=""
+            className="h-4 w-4 accent-lime-500   rounded cursor-pointer"
+          />
+          <label
+            for="remember-me-checkbox"
+            className="ml-2 text-sm font-medium text-black"
+          >
+            Remember me
+          </label>
+        </div>
+
         <button
           type="submit"
-          className="font-bold bg-green-600 w-full text-white rounded hover:bg-lime-500 hover:text-gray-800 py-2"
+          className="font-bold mt-5 bg-green-600 w-full text-white rounded hover:bg-lime-500 hover:text-gray-800 py-2"
         >
-          Login
+          Log in
         </button>
+
+        <div className="w-full pt-10 text-center text-green-600 font-bold hover:text-lime-500">
+          <a href="/">Forgot Password?</a>
+        </div>
+
+        <hr class="w-64 h-0.5 mx-auto my-2 bg-gray-300 border-0 rounded md:my-10 "></hr>
+
+        <p className="w-full mt-10 text-center">Dont have an account?</p>
+        <div className="w-full text-center text-green-600 font-bold hover:text-lime-500">
+          <a href="/">Sign up</a>
+        </div>
       </form>
     </div>
   );
