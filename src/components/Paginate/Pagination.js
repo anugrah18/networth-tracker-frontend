f627854931323433482db2b167125a22be2785fc
anugrah18 <anugrah18@gmail>
2023-05-01 21:36:35 -0700

    Styling improvements

--- src/components/Paginate/Pagination.js
+++ src/components/Paginate/Pagination.js
@@ -14,28 +14,36 @@ export default function Pagination(props) {
     <div className="w-full mx-auto  place-items-center ">
       <div className="">
         <ul className="my-10 flex items-center justify-left md:justify-center">
-          <li
-            onClick={() => paginate(firstPageIndex)}
-            className="text-lg mr-3 px-5 py-3 leading-tight   border  rounded-l-lg   bg-gray-800 border-gray-700 text-gray-300 hover:bg-gray-700 hover:text-white"
-          >
-            First
+          <li>
+            <a
+              href="#"
+              onClick={() => paginate(firstPageIndex)}
+              className="text-lg mr-3 px-5 py-3 leading-tight   border  rounded-l-lg   bg-gray-800 border-gray-700 text-gray-300 hover:bg-emerald-600 hover:border-emerald-600 hover:text-white cursor-pointer focus:bg-emerald-600 focus:border-emerald-600  focus:ring focus:ring-green-600"
+            >
+              First
+            </a>
           </li>
 
           {pageNumbers.map((pageNumber) => (
-            <li
-              onClick={() => paginate(pageNumber)}
-              key={pageNumber}
-              className="mr-3 px-5 py-3 leading-tight  bg-gray-800 border  border-gray-700 text-gray-300 hover:bg-gray-700 hover:text-white"
-            >
-              {pageNumber}
+            <li key={pageNumber}>
+              <a
+                href="#"
+                onClick={() => paginate(pageNumber)}
+                className="mr-3 px-5 py-3 leading-tight  bg-gray-800 border  border-gray-700 text-gray-300  hover:text-white hover:bg-emerald-600 hover:border-emerald-600 cursor-pointer focus:bg-emerald-600 focus:border-emerald-600  focus:ring focus:ring-green-600"
+              >
+                {pageNumber}
+              </a>
             </li>
           ))}
 
-          <li
-            onClick={() => paginate(lastPageIndex)}
-            className="text-lg px-5 py-3 leading-tight border  rounded-r-lg  bg-gray-800 border-gray-700 text-gray-300 hover:bg-gray-700 hover:text-white"
-          >
-            Last
+          <li>
+            <a
+              href="#"
+              onClick={() => paginate(lastPageIndex)}
+              className="text-lg px-5 py-3 leading-tight border  rounded-r-lg  bg-gray-800 border-gray-700 text-gray-300  hover:text-white hover:bg-emerald-600 hover:border-emerald-600 cursor-pointer focus:bg-emerald-600 focus:border-emerald-600  focus:ring focus:ring-green-600"
+            >
+              Last
+            </a>
           </li>
         </ul>
       </div>
